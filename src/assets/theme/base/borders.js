1ae28bb1ba5900a7b5547834e74bd97462942225
issachar-j <yisakoreyob9@gmail.com>
2024-06-03 19:37:29 +0300

    nn

--- src/assets/theme/base/borders.js
+++ src/assets/theme/base/borders.js
@@ -3,8 +3,8 @@
  * You can add new border width, border color or border radius using this file.
  * You can customized the borders value for the entire Material Kit 2 React using thie file.
  */ // Material Kit 2 React Base Styles
-import colors from "/theme/base/colors"; // Material Kit 2 React Helper Functions
-import pxToRem from "/theme/functions/pxToRem";
+import colors from "assets/theme/base/colors"; // Material Kit 2 React Helper Functions
+import pxToRem from "assets/theme/functions/pxToRem";
 const { grey } = colors;
 export default {
   borderColor: grey[300],
